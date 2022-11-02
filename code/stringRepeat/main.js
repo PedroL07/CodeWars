f41c137f4ee680ac740c2daeab73cf75d31adeb7
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-02 09:15:27 -0400

    String repeat

---
+++ code/stringRepeat/main.js
@@ -0,0 +1,16 @@
+// Description
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// My Code
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+// https://www.w3schools.com/jsref/jsref_repeat.asp
+
+// The repeat method allows you to take a string and repeat it as many time as you like
+
+// clever clode
+
+// My code is the same as the "clever" code
