3982ac465e9ff4ec9483b3d80485ef6a12b22f3f
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-06 23:19:42 -0500

    add five fix

---
+++ code/addFive/main.js
@@ -0,0 +1,22 @@
+// Description 
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+// My Code 
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
+
+
+// Thoughts 
+
+// simple code where it was (return num) and i fixed it by returning total
+
+
+// clever clode
+
+function addFive(num) {
+    return num + 5;
+  }
