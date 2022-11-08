a404b0649b3449a6a89a50e7ddecd9fca68bb92f
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-07 20:02:47 -0500

    Will you make it

---
+++ code/willYouMakeIt/main.js
@@ -0,0 +1,25 @@
+// Description
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+// My Code
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+  let newMiles = distanceToPump / mpg;
+
+  if (newMiles <= fuelLeft) {
+    return true;
+  }
+  if ((newMiles) => fuelLeft) {
+    return false;
+  }
+};
+
+// clever clode
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump / mpg <= fuelLeft;
+};
