f7ea5b3eeaa7a4b9a13793ec13ebead0efd0cd88
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-08 12:05:27 -0500

    Create main.js

---
+++ code/Bmi/main.js
@@ -0,0 +1,49 @@
+// Description 
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+// My Code 
+function bmi(weight, height) {
+    let tall = (height ** 2)
+    
+   let results = (weight / tall)
+   
+   if (results <= 18.5){
+     return "Underweight"
+   }
+     if (results <= 25){
+     return "Normal"
+   }
+     if (results <= 30){
+     return "Overweight"
+   }
+     if (results > 30){
+     return "Obese"
+   }
+  }
+
+
+
+// clever clode
+
+function bmi(weight, height) {
+    var result = weight/Math.pow(height,2) 
+    
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25) {
+      return "Normal";
+    } else if (result <= 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
+    }
+    
+  }
