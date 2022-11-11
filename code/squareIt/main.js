ae06acf4d0a7ebcccc9941c003b0ee3b20d17d4f
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-10 21:12:43 -0500

    number square

---
+++ code/squareIt/main.js
@@ -0,0 +1,12 @@
+// Description 
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My Code 
+function square(num) {
+    let randomSquared = (num ** 2); {
+        return randomSquared
+    }
+}
+
+// clever clode
+const square = (n) => n * n;
