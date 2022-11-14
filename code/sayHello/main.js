9f19ebcede57caf3e69b9dd0ebb14cc0d53837e1
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-13 22:52:54 -0500

    Say hello Debug

---
+++ code/sayHello/main.js
@@ -0,0 +1,12 @@
+// Description 
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// My Code 
+function sayHello(name) {
+    return ('Hello, ' + name)
+  }
+
+
+// clever clode
+const sayHello = name => `Hello, ${name}`;
+
