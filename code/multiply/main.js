b1da80e7616b3587514d9d50bdf0f9465f3bc5da
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-11-14 22:07:31 -0500

    Divisible and multiply

---
+++ code/multiply/main.js
@@ -0,0 +1,16 @@
+// Description
+// This code does not execute properly. Try to figure out why.
+
+// My Code
+function multiply(a, b) {
+  let sum = a * b;
+  return sum;
+}
+
+// Thoughts
+
+// clever clode
+
+multiply = function (a, b) {
+  return a * b;
+};
--- code/sayHello/main.js
+++ code/sayHello/main.js
@@ -1,5 +1,5 @@
 // Description 
-The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
 
 // My Code 
 function sayHello(name) {
