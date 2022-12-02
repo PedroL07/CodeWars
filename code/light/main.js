b0e0406827bbfd61ceee577e271e12802bf4575a
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-12-01 23:54:41 -0500

    light

---
+++ code/light/main.js
@@ -0,0 +1,13 @@
+// Description 
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// My Code 
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
+
+
