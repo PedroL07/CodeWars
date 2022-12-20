f12a3164899deaa58f5499d1a49bcfaf9fb187de
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-12-19 23:25:46 -0500

    Count Divisors

---
+++ code/countDivisior/main.js
@@ -0,0 +1,33 @@
+// Description 
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+// My Code 
+
+  function getDivisorsCnt(n){
+    // todo
+  
+  let arr = []
+  for (let i=1;i<=n;i++) {
+    n%i==0 ? arr.push(i) : {}
+  }
+  return arr.length
+}
+
+// clever clode
+
+function getDivisorsCnt(n) {
+    for (var d = 0, i = n; i > 0; i--) {
+      if (n % i == 0) d++;
+    }
+    
+    return d;
+  }
